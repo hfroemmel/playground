2b91ce13d0782e2a72fc43ce516223531059ffea
hfr <hendrik.froemmel@appsfactory.de>
2014-03-14 00:08:48 +0100

    Initial Commit

---
+++ playground/PGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PGAppDelegate.h
+//  playground
+//
+//  Created by Hendrik Frömmel on 14.03.14.
+//  Copyright (c) 2014 Hendrik Frömmel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ playground/PGViewController.m
@@ -0,0 +1,29 @@
+//
+//  PGViewController.m
+//  playground
+//
+//  Created by Hendrik Frömmel on 14.03.14.
+//  Copyright (c) 2014 Hendrik Frömmel. All rights reserved.
+//
+
+#import "PGViewController.h"
+
+@interface PGViewController ()
+
+@end
+
+@implementation PGViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
